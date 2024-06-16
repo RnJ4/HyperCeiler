351379926f43a50bd531489c852ec967983dd35d
RnJ4 <30072955+RnJ4@users.noreply.github.com>
2024-06-16 21:58:13 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
 
 
 val apkId = "HyperCeiler"
-val buildTypes = "release"
+val buildTypes = "debug"
 val roots = mapOf(
     "animation" to "libs/animation-${buildTypes}.aar",
     "appcompat" to "libs/appcompat-${buildTypes}.aar",
